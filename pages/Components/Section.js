88447021ff76df0cdda44511ddc8861120043312
Hopetown1 <jacob.whyte.jw@gmail.com>
2024-04-22 18:57:13 +0700

    Added Tina

--- pages/Components/Section.js
+++ pages/Components/Section.js
@@ -5,6 +5,8 @@ import styles from './Section.module.css';
 import ProjectDetails from './ProjectDetails'; // Assuming your new component
 
 
+
+
 const projectsData = [
     {
       title: "Niko",
@@ -90,66 +92,71 @@ const saraInfo = {
 }
 
 
+ 
+
   
+// Define the Section component
 const Section = () => {
+    // Define state variables for selected project, visibility of Sara's info, and project list
     const [selectedProject, setSelectedProject] = useState(null);
     const [showSaraInfo, setShowSaraInfo] = useState(false);
+    const [projectListVisible, setProjectListVisible] = useState(false); 
+
+    // Define a function to handle click events on Sara's info
     const handleSaraInfoClick = () => {
-      setShowSaraInfo(true); // Show Sara Info
-      setSelectedProject(null); // Reset selected project
-  };
+      setShowSaraInfo(true); // Set visibility of Sara's info to true
+      setSelectedProject(null); // Reset the selected project
+    };
+
+    // Define a function to handle click events on a project
     const handleProjectClick = (project) => {
-      setShowSaraInfo(false);
-      setSelectedProject(project); 
-  };
-  return (
-    
-    <section className={styles.section}>
-      
-      <div className={styles.column_1}> 
+      setShowSaraInfo(false); // Set visibility of Sara's info to false
+      setSelectedProject(project); // Set the selected project
+    };
 
-      <h1 
-      style={{ color: showSaraInfo === true ? 'rgba(0, 0, 0, 0.4)' : '' }} 
-      className={styles.h1} 
-      onClick={() => handleSaraInfoClick()}>Sara Barcons
-      </h1> 
+    // Define a function to handle click events on the circle
+    const handleCircleClick = () => {
+      setProjectListVisible(!projectListVisible); // Toggle visibility of project list
+      console.log('projectListVisible:', projectListVisible) // Log the current visibility state of project list
+    };
 
-
-
-
-      <div className={styles.projectList}>
-       
-          {projectsData.map((project) => (
-            <ul className={styles.projectItem} key={project.title} onClick={() => handleProjectClick(project)}> 
-            <article className={styles.project}>
-            <p 
-              style={{ color: selectedProject === project ? 'rgba(0, 0, 0, 0.4)' : '' }} 
-              className={styles.p}
-   > 
-              {project.title} | {project.brand} | {project.date}</p>
-            </article>
-            </ul>
-  
-          ))}
+    // Return the JSX to render for this component
+    return (
+      <section className={styles.section}>
+        <div className={styles.green_circle} onClick={handleCircleClick}></div>
+        <div className={styles.column_1}> 
+          <h1 
+            style={{ color: showSaraInfo === true ? 'rgba(0, 0, 0, 0.4)' : '' }} 
+            className={styles.h1} 
+            onClick={() => handleSaraInfoClick()}>Sara Barcons
+          </h1> 
+          <div className={`${styles.projectList} ${projectListVisible ? 'show' : ''}`}>
+            {projectsData.map((project) => (
+              <ul className={styles.projectItem} key={project.title} onClick={() => handleProjectClick(project)}> 
+                <article className={styles.project}>
+                  <p 
+                    style={{ color: selectedProject === project ? 'rgba(0, 0, 0, 0.4)' : '' }} 
+                    className={styles.p}
+                  > 
+                    {project.title} | {project.brand} | {project.date}</p>
+                </article>
+              </ul>
+            ))}
+          </div>
         </div>
-      </div>
-
-
-      <div className={styles.column_2}>  
-      {showSaraInfo ? (
-        <div>
-           <p className={styles.text_1}>{saraInfo.text_1}</p>
-           <p className={styles.text_2_line1}>{saraInfo.text_2_line1} </p>
-           <p className={styles.text_2_line2}>{saraInfo.text_2_line2}</p>
-        </div> 
-    ) : (
-        selectedProject && <ProjectDetails project={selectedProject} /> 
-    )}
-
-      </div>
-
-    </section>
-  );
+        <div className={styles.column_2}>  
+          {showSaraInfo ? (
+            <div>
+              <p className={styles.text_1}>{saraInfo.text_1}</p>
+              <p className={styles.text_2_line1}>{saraInfo.text_2_line1} </p>
+              <p className={styles.text_2_line2}>{saraInfo.text_2_line2}</p>
+            </div> 
+          ) : (
+            selectedProject && <ProjectDetails project={selectedProject} /> 
+          )}
+        </div>
+      </section>
+    );
 };
-
 export default Section;
+
