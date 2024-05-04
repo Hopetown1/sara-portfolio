726a08ea8e36329fd4c28ed7f5ff86e3fe0791dc
Hopetown1 <jacob.whyte.jw@gmail.com>
2024-05-04 15:08:20 +0700

    using environment variables in client

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -2,10 +2,17 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from '../env'
 
-export const client = createClient({
-  projectId: 'lspp38nx', // you can find this in sanity.json
-  dataset: 'sara_info', // or the name of your dataset
-  apiVersion: '2021-03-25', // use a UTC date string
-  useCdn: true, // `false` if you want to ensure fresh data
-});
+//Local only:
+// export const client = createClient({
+//   projectId: 'lspp38nx', // you can find this in sanity.json
+//   dataset: 'sara_info', // or the name of your dataset
+//   apiVersion: '2021-03-25', // use a UTC date string
+//   useCdn: true, // `false` if you want to ensure fresh data
+// });
 
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn, 
+});
